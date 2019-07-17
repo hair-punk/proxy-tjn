7b43856a1e69f5ae247364e9e1593dd42dff3bff
chokoreetosan <mallepalliabhishek@gmail.com>
2019-07-16 23:12:38 -0700

    server side rendering stil works

--- server/server.js
+++ server/server.js
@@ -3,13 +3,15 @@ const bodyParser = require('body-parser')
 const React = require('react')
 const ReactDOMServer = require('react-dom/server')
 const request = require("request")
+const path = require("path");
 const app = express();
+const fs = require('fs');
 const PORT = 3000
 
 app.use(bodyParser.json())
 app.use(express.urlencoded())
-// app.use(express.static('public'))
-app.use( function(req, res, next) {
+app.use(express.static(path.join(__dirname,'public')))
+ app.use( function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*")
 	res.header("Access-Control-Allow-Headers", "origin, xRequested-With, Content-Type, Accept")
 	next();
@@ -19,20 +21,27 @@ app.use( function(req, res, next) {
 // 	res.sendFile(path.join(__dirname + '/public/index.html'))
 // })
 const forward = function (req,res){
-	console.log('get request recieved')
-	var payload = '<!DOCTYPE html><html><head\></head\><body\><div id="app"></div></body\></html>';
+	console.log(req.method,' request recieved')
+
 	request('http://localhost:3006/'+Math.floor(Math.random() * 10000000),(err,response, body)=>{
-		// console.log(err);
-		// console.log('statuscode', response.statusCode)
-		// console.log(body);
-		//  return res.send(payload.replace('<div id="app"></div>', '<div id="app">'+ReactDOMServer.renderToString(<App />)+'</div>' )
-		res.send(payload.replace('<div id="app"></div>',body))
+
+		 console.log('statuscode', response.statusCode)
+
+		fs.writeFile('/home/abhi/proxy-tjn/public/index.html',`<html><head></head><body">${body}</body></html>`,(err)=>{
+			if(err) {console.log('there was an error', err)}
+			else{
+			console.log('write successful');
+			res.sendFile('/home/abhi/proxy-tjn/public/index.html')
+			}
+		})
+
+		// res.send(payload.replace('<div id="app"></div>',body))
 		// )
 	;
 });
 }
 
-app.use("/", forward);
+ app.get("/", forward);
 
 app.listen(PORT, ()=> {
 	console.log('tjn-proxy Server listening on port ',  PORT)
