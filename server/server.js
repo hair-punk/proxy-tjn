e21948efd5e66221bd652fd35ad6d995a24a0ce9
Ubuntu <ubuntu@ip-172-31-1-228.us-west-1.compute.internal>
2019-07-18 10:28:14 +0000

    refacted to work without server side rendering

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = 3000
 
 app.use(bodyParser.json())
 app.use(express.urlencoded())
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(__dirname +'/public'))
  app.use( function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*")
 	res.header("Access-Control-Allow-Headers", "origin, xRequested-With, Content-Type, Accept")
@@ -21,28 +21,30 @@ app.use(express.static(path.join(__dirname,'public')))
 // 	res.sendFile(path.join(__dirname + '/public/index.html'))
 // })
 const forward = function (req,res){
-	console.log(req.method,' request recieved')
 
-	request('http://localhost:3006/'+Math.floor(Math.random() * 10000000),(err,response, body)=>{
+request('http://52.53.189.14:3006/', (error,response, body)=>{
+//console.log(body);
+res.send(body)
+})
 
-		 console.log('statuscode', response.statusCode)
-
-		fs.writeFile('/home/abhi/proxy-tjn/public/index.html',`<html><head></head><body">${body}</body></html>`,(err)=>{
-			if(err) {console.log('there was an error', err)}
-			else{
-			console.log('write successful');
-			res.sendFile('/home/abhi/proxy-tjn/public/index.html')
-			}
-		})
-
-		// res.send(payload.replace('<div id="app"></div>',body))
+//	console.log(req.method,' request recieved')
+//	request('http://52.53.189.14:3006/'),(err,response, body)=>{
+//		res.send(body)
+//		 console.log('statuscode', response.statusCode)
+//		fs.writeFile('/home/ubuntu/proxy-tjn/public/index.html',`<html><head></head><body">${body}</body></html>`,(err)=>{
+//			if(err) {console.log('there was an error', err)}
+//			else{
+//			console.log('write successful');
+//			res.sendFile('/home/ubuntu/proxy-tjn/public/index.html')
+//			}
 		// )
-	;
-});
+//	};
 }
 
- app.get("/", forward);
-
+app.get("/", forward);
+app.get('/loaderio-55ffdfbfc38d2c44bf172c54c695ca28',(req,res)=>{
+res.send('loaderio-55ffdfbfc38d2c44bf172c54c695ca28')
+})
 app.listen(PORT, ()=> {
 	console.log('tjn-proxy Server listening on port ',  PORT)
 })
